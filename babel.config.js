6645961ed7a1a38fe92b8ecfc0e2d5a9881cdfb9
DucKhaiGit <tomvnoki@gmail.com>
2024-10-21 01:11:29 +0700

    Delete all files in the test branch

--- babel.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  presets: ["@babel/preset-env", "@babel/preset-react"],
-  plugins: [
-    "@babel/plugin-proposal-private-property-in-object", // Nếu bạn đã thêm gói này
-  ],
-};
